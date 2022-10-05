98aa3a8c7d7c2ee7fb559b61fa3a8ecd8731854b
Ranger-HK <maxravindu99@gmail.com>
2022-10-05 17:14:29 +0530

    updated

---
+++ Abc.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View,Text } from "react-native";
+import Test1 from "./Test1";
+
+function Abc(){
+    return(
+        <View>
+            <Text>Hello How Are You</Text>
+            <Test1/>
+        </View>
+
+    );
+}
+
+export default Abc;
