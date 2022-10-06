330161cadecf0f1446ef280b7539fe2b1b0c454c
Ranger-HK <maxravindu99@gmail.com>
2022-10-06 23:15:50 +0530

    Updated

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View,Text } from "react-native";
 import Test1 from "./Test1";
 import Abc from "./Abc";
+import Hi from "./Hi";
 
 function App(){
   return(
@@ -9,6 +10,8 @@ function App(){
       <Text>Hello Welocme Mr Ravindu Prathibha </Text>
      <Test1/>
      <Abc/>
+     <Hi/>
+    
     </View>
   );
 }
