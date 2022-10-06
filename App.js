82f947f147218020f398b9ea5a0bf7ceb7ca9697
Ranger-HK <maxravindu99@gmail.com>
2022-10-07 02:11:06 +0530

    Updated

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { View,Text } from "react-native";
+import { View,Text,Button } from "react-native";
 import Test1 from "./Test1";
 import Abc from "./Abc";
 import Hi from "./Hi";
+import { Bubbles, DoubleBounce, Bars, Pulse } from 'react-native-loader';
+
 
 function App(){
   return(
@@ -11,9 +13,18 @@ function App(){
      <Test1/>
      <Abc/>
      <Hi/>
-    
+     <Button
+  title="Learn More"
+  color="#841584"
+  accessibilityLabel="Learn more about this purple button"
+/>
+
+<Bubbles size={10} color="#16a085" />
+
+
     </View>
-  );
+    
+  ); 
 }
 
 export default App;
